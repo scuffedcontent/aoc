ab216c05260068fcdf739f6b659f3705b513293a
acity609 <scuffedcontent@gmail.com>
2021-12-01 14:32:29 -0500

    added day1.py 2021 part_one solution

--- aoc/2021/day1/day1.py
+++ aoc/2021/day1/day1.py
@@ -1,1 +1,16 @@
 # Advent of Code 2021 Python
+
+
+def part_one(lines):
+    count=0
+    for i in range(1, len(lines)):
+        if lines[i] > lines[i-1]:
+            count+=1
+    return count
+
+with open("input.txt") as file:
+    inputs = [int(line) for line in file]
+
+print('Part One answer : ', part_one(inputs))
+
+
