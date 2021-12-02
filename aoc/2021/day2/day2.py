4f3666d9b44eaaf66a61db9189b9a8b0a79663d4
acity609 <scuffedcontent@gmail.com>
2021-12-02 11:17:11 -0500

    added day2.py part_two solution

--- aoc/2021/day2/day2.py
+++ aoc/2021/day2/day2.py
@@ -18,8 +18,24 @@ def part_one(lines):
             y+=amount
     return x * y
 
+def part_two(lines):
+    x,y,a=0,0,0
+    for line in lines:
+        line=line.split(" ")
+        amount=int(line[1])
+        direction=(line[0])
+        if direction == "up":
+            a-=amount
+        if direction == "down":
+            a+=amount
+        if direction == "forward":
+            x+=amount
+            y+=(a*amount)
+    print(x,y,a)
+    return x*y
 
 with open("input.txt") as file:
     data = file.read().splitlines()
 print(part_one(data))
+print(part_two(data))
 
