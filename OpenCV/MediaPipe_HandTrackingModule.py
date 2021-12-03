1220bde95ff3d27e2b1f975c5bf46f05cac8a215
acity609 <scuffedcontent@gmail.com>
2021-12-03 07:31:11 -0500

    Fixes

--- OpenCV/MediaPipe_HandTrackingModule.py
+++ OpenCV/MediaPipe_HandTrackingModule.py
@@ -11,8 +11,7 @@ with mp_hands.Hands(
     max_num_hands=2,
     min_detection_confidence=0.5) as hands:
   for idx, file in enumerate(IMAGE_FILES):
-    # Read an image, flip it around y-axis for correct handedness output (see
-    # above).
+    # Read an image, flip it around y-axis for correct handedness output.
     image = cv2.flip(cv2.imread(file), 1)
     # Convert the BGR image to RGB before processing.
     results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
