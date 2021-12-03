8202f9936704698d316def56209f7b9582b169a7
acity609 <scuffedcontent@gmail.com>
2021-12-03 07:29:36 -0500

    added Day3.py Part_One solution

---
+++ aoc/2021/day3/day3.py
@@ -0,0 +1,26 @@
+
+
+def part_one(lines):
+    G,E='',''
+    for idx in range(0, len(lines[0])):
+        O,Z=0,0
+        for line in range(0, len(lines)):
+            if lines[line][idx] == "0":
+                Z+=1
+            else:
+                O+=1
+        if Z>O:
+            G+='0'
+            E+='1'
+        else:
+            G+='1'
+            E+='0'
+    gamma = int(G,2)
+    epsilon = int(E, 2)
+    return gamma*epsilon
+
+
+with open("input.txt") as file:
+    input = [x for x in file.read().split()]
+
+print("Part One answer : ", part_one(input))
