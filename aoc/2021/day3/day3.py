daf5bf4c2ea06cf9f61684ca78622c8f0e979d78
acity609 <scuffedcontent@gmail.com>
2021-12-04 19:40:12 -0500

    Added day4 solutions Part_one, part_two

--- aoc/2021/day3/day3.py
+++ aoc/2021/day3/day3.py
@@ -19,8 +19,54 @@ def part_one(lines):
     epsilon = int(E, 2)
     return gamma*epsilon
 
+def part_two_oxygen(lines):
+    idx=0
+    while len(lines)>1:
+        O,Z=0,0
+        Ones=[]
+        Zeroes=[]
+        for line in range(0, len(lines)):
+            if lines[line][idx] == "0":
+                Z+=1
+                Zeroes.append(lines[line])
+            else:
+                O+=1
+                Ones.append(lines[line])
+        if Z>O:
+            lines=Zeroes
+        else:
+            lines=Ones
+        idx+=1
+    oxygen= int(lines[0],2)
+    return oxygen
+    print(oxygen)
+def part_two_CO2(lines):
+    idx=0
+    while len(lines)>1:
+        O,Z=0,0
+        Ones=[]
+        Zeroes=[]
+        for line in range(0, len(lines)):
+            if lines[line][idx] == "0":
+                Z+=1
+                Zeroes.append(lines[line])
+            else:
+                O+=1
+                Ones.append(lines[line])
+        if O<Z:
+            lines=Ones
+        else:
+            lines=Zeroes
+        idx+=1
+    CarbonDioxide = int(lines[0],2)
+    return CarbonDioxide
+    print(CarbonDioxide)
 
+    
+    return part_one(lines)
 with open("input.txt") as file:
     input = [x for x in file.read().split()]
 
 print("Part One answer : ", part_one(input))
+print("Part Two answer : ", part_two_oxygen(input))
+print("Part Two answer : ", part_two_CO2(input))
