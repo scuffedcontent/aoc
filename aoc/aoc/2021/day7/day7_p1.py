bc4bddc3b0df962b44e88f1692071c804a2b4f1b
scuffedcontent <scuffedcontent@gmail.com>
2021-12-08 07:01:50 -0500

    added day8.py part one

--- aoc/aoc/2021/day7/day7_p1.py
+++ aoc/aoc/2021/day7/day7_p1.py
@@ -5,3 +5,18 @@ Day 7 part One
 Python
 
 '''
+
+import sys
+
+with open("input.txt") as file:
+    data = [int(x) for x in file.readline().split(",")]
+
+least = sys.maxsize
+for stop in range(0, 10000):
+    value = 0 
+    for element in data:
+        value += abs(element - stop)
+    if value < least:
+        least = value
+
+print('Part one solution : ', least)
--- aoc/aoc/2021/day7/day7_p2.py
+++ aoc/aoc/2021/day7/day7_p2.py
@@ -5,3 +5,20 @@ Day 7 part Two
 Python
 
 '''
+
+import sys
+#from functools import cache
+
+with open('input.txt') as file:
+    data = [int(x) for x in file.readline().split(',')]
+
+def sumUp(x):
+    return x * (x - 1) // 2
+least = sys.maxsize
+for stop in range(0, 10000):
+    value = 0
+    for element in data:
+        value += sumUp(abs(element - stop)+1)
+    if value < least:
+        least = value
+print("part two solution : ", least)
