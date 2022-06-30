675cf475ae92bbaa105de1d1818140e85df89b87
Scuffed_Content <scuffedcontent@gmail.com>
2022-06-30 14:47:33 -0400

    clean up

---
+++ leetcode/Duplicates.py
@@ -0,0 +1,7 @@
+nums = [1, 2, 3, 1]
+seen = set()
+for i in nums:
+    if i in seen:
+        print("True")
+    seen.add(i)
+print("False")
---
+++ leetcode/Roman_to_Integer.py
@@ -0,0 +1,11 @@
+s = 'MLIV'
+
+roman =  {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+solution = 0
+
+for i in range(len(s)):
+    if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+        solution -= roman[s[i]]
+    else:
+        solution += roman[s[i]]
+    print(solution)
