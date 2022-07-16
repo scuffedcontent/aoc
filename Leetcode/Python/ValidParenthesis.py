be73e95fb674bbe02e51d332b18930194369e976
Scuffed_Content <scuffedcontent@gmail.com>
2022-07-16 13:35:51 -0400

    validate errors

--- Leetcode/Python/ValidParenthesis.py
+++ Leetcode/Python/ValidParenthesis.py
@@ -4,15 +4,17 @@ Leetcode
 Validate Parenthesis
 
 '''
-s = str
-onStack = []
-isClosed = {')':'(', ']':'[','}':'{'}
-for c in s:
-    if c in isClosed:
-        if onStack and onStack[-1] == isClosed[c]:
-            onStack.pop()
+class Solution:
+    def validateParenthesis():
+        s = str
+        onStack = []
+        isClosed = {')':'(', ']':'[','}':'{'}
+        for c in s:
+            if c in isClosed:
+                if onStack and onStack[-1] == isClosed[c]:
+                    onStack.pop()
+            else:
+                return False
         else:
-            return False
-    else:
-        onStack.append(c)
-return True if not onStack else False
+            onStack.append(c)
+        return True if not onStack else False
