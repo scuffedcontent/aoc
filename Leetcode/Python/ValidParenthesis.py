7bdfcc191fd13c3c3749ada3323c73ceae1ac89e
Scuffed_Content <scuffedcontent@gmail.com>
2022-07-16 13:25:42 -0400

    1337C0D3

--- Leetcode/Python/ValidParenthesis.py
+++ Leetcode/Python/ValidParenthesis.py
@@ -1,6 +1,18 @@
-validTest="()"
-str="()}]a"
-valid="(){}[]"
-for i in range(0, len(str)):
-    if str[i] in valid:
-        print("True")
+
+'''
+Leetcode
+Validate Parenthesis
+
+'''
+s = str
+onStack = []
+isClosed = {')':'(', ']':'[','}':'{'}
+for c in s:
+    if c in isClosed:
+        if onStack and onStack[-1] == isClosed[c]:
+            onStack.pop()
+        else:
+            return False
+    else:
+        onStack.append(c)
+return True if not onStack else False
