c8623c58d98cdefb6e86adb8e6159a2a2f23dec8
Scuffed_Content <scuffedcontent@gmail.com>
2022-07-30 19:07:14 -0400

    work

--- ReactAppTS/rts/src/index.tsx
+++ ReactAppTS/rts/src/index.tsx
@@ -1,12 +1,7 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const App = () => {
-    return (
-    <div>
-        <h1>Hello World!</h1>
-        </div>
-           );
-};
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
