727dfff087b3ecf2e2fc297a172ecf2b6a3fdffa
Scuffed_Content <scuffedcontent@gmail.com>
2022-08-15 14:46:48 -0400

    summary ranges leetcode

--- Leetcode/Python/SummaryRanges.py
+++ Leetcode/Python/SummaryRanges.py
@@ -9,7 +9,6 @@ Each range [a,b] in the list should be output as:
 Input: nums = [0,1,2,4,5,7]
 Output: ["0->2","4->5","7"]
 Explanation: The ranges are:
-[0,2] --> "0->2"
 [4,5] --> "4->5"
 [7,7] --> "7"
 
