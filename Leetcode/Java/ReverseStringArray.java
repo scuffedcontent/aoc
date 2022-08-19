dc8ac021e9c71f3faed721357c40687b4e05bb5f
Scuffed_Content <scuffedcontent@gmail.com>
2022-08-19 09:09:33 -0400

    l33tc0d3

--- Leetcode/Java/ReverseStringArray.java
+++ Leetcode/Java/ReverseStringArray.java
@@ -4,36 +4,28 @@
  * example output: {'o', 'l', 'l', 'e', 'h'}
  */
 
-
-
-
-
-class Solution{
+class Solution {
     public static void main(Char[] Array) {
         char temp;
-        for (int i=0; i<Array.length/2;i++) {
+        for (int i = 0; i < Array.length / 2; i++) {
             temp = Array[i];
-            Array[i] = Array[Array.length-i-1];
-            Array[Array.length-i-1] = temp;
+            Array[i] = Array[Array.length - i - 1];
+            Array[Array.length - i - 1] = temp;
         }
         System.out.println(Array);
     }
 }
 
-
-
 class BlazinglyFastSolution {
     public void reverseString(char[] s) {
-        int i=0;
-        int j=s.length-1;
-        while(i<j){
-            char temp=s[i];
-            s[i]=s[j];
-            s[j]=temp;
+        int i = 0;
+        int j = s.length - 1;
+        while (i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
             i++;
             j--;
         }
     }
 }
-
-
