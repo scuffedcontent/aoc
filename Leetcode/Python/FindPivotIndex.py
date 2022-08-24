51f7fbd9b6e1b78652ef8d28330176c7c4600eb8
scuffedcontent <scuffedcontent@gmail.com>
2022-08-24 09:25:49 -0400

    l33tc0d3

---
+++ Leetcode/Python/FindPivotIndex.py
@@ -0,0 +1,18 @@
+'''
+
+
+
+'''
+
+
+haystack=['h','e','a','a','a','l','l','g']
+needle =['l','l','g']
+
+
+l=len(needle)
+
+for i in range(len(haystack)):
+    # this a placeholder. It is not correct
+    if needle in haystack:
+        print('true')
+print('false')
