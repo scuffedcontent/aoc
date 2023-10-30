acd25e6561a712a9797089c0ac41c9f9e33b6e07
scuffedcontent <scuffedcontent@gmail.com>
2023-10-29 20:41:29 -0400

    lgtm

--- hello_world/target/debug/deps/hello_world-7fa204f8dfe7ae50.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/plump/work/git_repos/hello_world/target/debug/deps/hello_world-7fa204f8dfe7ae50.rmeta: src/main.rs
-
-/mnt/c/Users/plump/work/git_repos/hello_world/target/debug/deps/hello_world-7fa204f8dfe7ae50.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/plump/work/git_repos/hello_world/target/debug/hello_world: /mnt/c/Users/plump/work/git_repos/hello_world/src/main.rs
